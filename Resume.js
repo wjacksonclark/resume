86a1b4f86b1bb60deaa3e06d6d14757aff2455b5
wjacksonclark <112517769+wjacksonclark@users.noreply.github.com>
2022-08-30 17:04:04 -0400

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,54 @@
+website.component("resume", {
+  /*html*/
+  template: `
+    <div id="profile">
+                <img id="myProfile" src="./profile.jpg" alt="Profile of William Clark (myself)">
+        
+                <div id="personalInformation">
+                    <a href="mailto:Wjacksonclark13@gmail.com"
+                    title="Personal Email"
+                    >Wjacksonclark13@gmail.com&nbsp;|</a>
+
+                    <a href="tel:404-452-6286"
+                    title="Personal Number"
+                    >&nbsp;Mobile: 404-452-6286</a>
+                </div>
+        
+                <div id="name">
+                    <h1>William Clark<h1>
+                </div>
+
+                <div id="circulars">
+                   <div id="objective">
+                        <h2>Objective</h2>
+                        <p>• To have the opportunity to improve a company I believe in. And to continue developing my abilities within a comfortable work environment.</p><br>
+                    </div>
+
+                    <div id="education">
+                        <h2>Education</h2>
+                        <p>• Bachelor of Computer Science degree from the University of West Georgia. (2019-Present)</p>
+                        <p>• Comfortable in Java/HTML & CSS, familiar with python, c#, javascript</p><br>
+                    </div>
+
+                    <div id="achievements">
+                        <h2>Achievements</h2>
+                        <p>• 2019 Perseverance award for Information Technology at the Carroll County College and Career Academy<br>
+                        • President’s list for 2 semesters at the University of West Georgia<br>
+                        • Developed website for University of West Georgia’s Association for Computing Machinery<br>
+                        • Create Grocery Frenzy mini game off of Scratch<br>
+                        • Created program to decipher encrypted messages using Java<br>
+                        </p><br>
+                    </div>
+
+                    <div id="activities">
+                        <h2>Activities/Interests</h2>
+                        <p>• Student member, Future Business Leaders of America<br>
+                        • Teacher’s Assistant for Computer Science<br>
+                        • University of West Georgia’s ACM Club position: Technical Engineer/Event Coordinator<br>
+                        • Writing, Developing, Exercising<br>
+                        </p>
+                    </div>
+                </div>
+        </div>
+    `,
+});
